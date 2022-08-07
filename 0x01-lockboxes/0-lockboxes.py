691f91852b7e3f03c8b49056e67f7409937fbe35
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-07 18:46:13 +0300

    fix error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,7 +11,7 @@ def canUnlockAll(boxes):
 
     if len(boxes[0]) == 0:
         return False
-    
+
     class Box:
         def __init__(self, open, keys):
             self.open = open
@@ -23,19 +23,20 @@ def canUnlockAll(boxes):
         list_of_boxes.append(b)
 
     list_of_boxes[0].open = True
-    
-    for box in list_of_boxes:
-        if (box.open == True) and box != []:
-            for i in box.keys:
-                # print(i)
-                list_of_boxes[i].open = True
-                if (i < list_of_boxes.index(box)):
-                    for x in list_of_boxes[i].keys:
-                        list_of_boxes[x].open = True
 
     for box in list_of_boxes:
-        if box.open == False:
+        if (box.open is True) and box != []:
+            for i in box.keys:
+                # print(i)
+                if (i < len(list_of_boxes)):
+                    list_of_boxes[i].open = True
+                    if (i < list_of_boxes.index(box)):
+                        for x in list_of_boxes[i].keys:
+                            if (x < len(list_of_boxes)):
+                                list_of_boxes[x].open = True
+
+    for box in list_of_boxes:
+        if box.open is False:
             return False
 
     return True
-
