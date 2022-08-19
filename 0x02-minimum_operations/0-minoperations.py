73f04532c4678c025b465db920b2b5115b1897da
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-19 16:55:32 +0300

    fix pep8 validation errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,20 +4,21 @@ import math
 
 
 def minOperations(n):
-    """Returns the minimum number of operations needed to copy and paste a string n times"""
+    """Returns the minimum number of operations needed to
+    copy and paste a string n times"""
 
     my_list = []
-    #even number divisible
+    # even number divisible
     while n % 2 == 0:
         my_list.append(2)
         n = n / 2
-    
-    #n became odd
-    for i in range(3,int(math.sqrt(n))+1,2):
+
+    # n became odd
+    for i in range(3, int(math.sqrt(n))+1, 2):
         while (n % i == 0):
             my_list.append(int(i))
             n = n / i
-    
+
     if n > 2:
         my_list.append(int(n))
 
