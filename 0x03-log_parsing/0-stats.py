39675d98db4a2b3f2788c70e5fe5c63eff3b3c7a
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-19 19:28:37 +0300

    fix errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+"""Parses log from standard input"""
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+def parse_logs():
+    """Parses log from stdin"""
+    accept_codes = {
+        200: 0, 301: 0,
+        400: 0, 401: 0,
+        403: 0, 404: 0,
+        405: 0, 500: 0
+        }
+    try:
+        line_num = 0
+        file_size = 0
+        # status_codes = {}
+        for line in sys.stdin:
+            line_num += 1
+            tokens = line.split(" ")[-2:]
+            file_size += int(tokens[1])
+            code = tokens[0]
+            if (code).isdigit() and int(code) in accept_codes.keys():
+                accept_codes[int(code)] = accept_codes.get(int(code)) + 1
+            keys = list(accept_codes.keys())
+            # status_codes.sort()
+            if line_num > 9:
+                line_num = 0
+                print("File size: {}".format(file_size))
+                [print("{}: {}".format(i, accept_codes.get(i))) for i in keys
+                    if accept_codes.get(i) > 0]
+    except Exception as e:
+        # [print("{}: {}".format(i, status_codes.get(i))) for i in keys]
+        [print("{}: {}".format(i, accept_codes.get(i))) for i in keys
+            if accept_codes.get(i) > 0]
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
 
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+if __name__ == "__main__":
+    parse_logs()
