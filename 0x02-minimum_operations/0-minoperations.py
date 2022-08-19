2704f3b50d0919c57c12f5dd99f62b5e99849b2c
Beldine-Moturi <beldinemoturi@gmail.com>
2022-08-19 16:52:41 +0300

    return sum of prime factors of a number

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a function"""
+import math
+
+
+def minOperations(n):
+    """Returns the minimum number of operations needed to copy and paste a string n times"""
+
+    my_list = []
+    #even number divisible
+    while n % 2 == 0:
+        my_list.append(2)
+        n = n / 2
+    
+    #n became odd
+    for i in range(3,int(math.sqrt(n))+1,2):
+        while (n % i == 0):
+            my_list.append(int(i))
+            n = n / i
+    
+    if n > 2:
+        my_list.append(int(n))
+
+    return sum(my_list)
