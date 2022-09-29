3fca4816eac0015c85446c372e0684123d52aa57
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-29 20:51:58 +0300

    semistandard styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,23 +5,23 @@ const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
 request.get(url, (err, response, body) => {
-    if (err) {
-	console.log('1:', err);
-    } else if (response.statusCode === 200) {
-	const characters = JSON.parse(body).characters;
-	for (const i in characters) {
-	    request.get(characters[i], (err, response, body) => {
-		if (err) {
-		    console.log(err);
-		} else if (response.statusCode === 200) {
-		    const name = JSON.parse(body).name;
-		    console.log(name);
-		} else {
-		    console.log(`Error code: ${response.statusCode}`);
-		}
-	    });
-	}
-    } else {
-	console.log(`Error code 1: ${response.statusCode}`);
+  if (err) {
+    console.log('1:', err);
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const i in characters) {
+      request.get(characters[i], (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          const name = JSON.parse(body).name;
+          console.log(name);
+        } else {
+          console.log(`Error code: ${response.statusCode}`);
+        }
+      });
     }
+  } else {
+    console.log(`Error code 1: ${response.statusCode}`);
+  }
 });
