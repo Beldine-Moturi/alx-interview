d3086f952b4cae4fb4c936bca3cef7159c781290
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-29 20:53:39 +0300

    semistandard styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-request.get(url, (err, response, body) => {
-  if (err) {
-    console.log('1:', err);
-  } else if (response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    for (const i in characters) {
-      request.get(characters[i], (err, response, body) => {
-        if (err) {
-          console.log(err);
-        } else if (response.statusCode === 200) {
-          const name = JSON.parse(body).name;
-          console.log(name);
-        } else {
-          console.log(`Error code: ${response.statusCode}`);
-        }
-      });
-    }
-  } else {
-    console.log(`Error code 1: ${response.statusCode}`);
-  }
+const id = process.argv[2];
+
+const baseUrl = 'https://swapi-api.hbtn.io/api/';
+
+request.get(`${baseUrl}films/${id}`, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  const result = body.characters;
+  // console.log(result);
+
+  listActors(result);
 });
+
+function listActors (result, i = 0) {
+  if (i === result.length) return;
+
+  request(result[i], { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+
+    console.log(body.name);
+    listActors(result, i + 1);
+  });
+}
